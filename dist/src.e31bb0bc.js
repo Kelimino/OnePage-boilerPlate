ea6d4b8b1fd9ec6f4758b56294441b0246c342cb
kelimino <jourdren.kellig@gmail.com>
2021-04-12 16:05:33 +0200

    init

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -10697,7 +10697,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49366" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56249" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
